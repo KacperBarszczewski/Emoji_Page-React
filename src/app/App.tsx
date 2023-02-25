f50964f8196f5b1a74c5ad8f9632a62e8bf9b476
KacperB <kacpero.barszczewski@gmail.com>
2023-02-25 19:34:14 +0100

    feat: add dark mode and set this in localstory

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
+import useColorMode from '../hooks/useColorMode'
 
 export function App() {
 
+  const [colorMode, setColorMode]= useColorMode();
 
   return (
-    <h1 className="text-3xl font-bold">
-      Hello &#129303;
-    </h1>
+    <div className='w-screen h-screen bg-slate-400 dark:bg-slate-700 '>
+      <h1 className="text-3xl font-bold ">
+        Hello &#129303;
+      </h1>
+      <button className='bg-orange-400'
+      onClick={()=>setColorMode(colorMode==='light'?'dark':'light')}>
+        Change color
+      </button>
+    </div>
   )
 }
 
