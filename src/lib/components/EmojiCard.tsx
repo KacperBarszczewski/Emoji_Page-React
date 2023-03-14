2e35a4c43ae81f6295d4b65df7faf3e08fcf852c
KacperB <kacpero.barszczewski@gmail.com>
2023-03-14 14:44:27 +0100

    feat: semibold on category names

--- src/lib/components/EmojiCard.tsx
+++ src/lib/components/EmojiCard.tsx
@@ -12,11 +12,11 @@ export const EmojiCard = ({ emojiDetails }: EmojiDetails) => {
         <div className=" min-h-[400px]  w-64  bg-foreground  p-3 flex flex-col items-center rounded-md ">
             <div className="text-8xl h-40 w-40 bg-typography-dark rounded-md flex items-center justify-center" dangerouslySetInnerHTML={{ __html: htmlCode[0] }} />
             <div className="grid grid-cols-3 gap-2 mt-4 text-typography-dark">
-                <div>Name:</div><div className="col-span-2 ">{name}</div>
-                <div>category:</div><div className="col-span-2">{category}</div>
-                <div>group:</div><div className="col-span-2">{group}</div>
-                <div>htmlCode:</div><div className="col-span-2">{htmlCode[0]}</div>
-                <div>unicode:</div><div className="col-span-2"> {unicode[0]}</div>
+                <div className="font-semibold">Name:</div><div className="col-span-2 ">{name}</div>
+                <div className="font-semibold">Category:</div><div className="col-span-2">{category}</div>
+                <div className="font-semibold">Group:</div><div className="col-span-2">{group}</div>
+                <div className="font-semibold">HtmlCode:</div><div className="col-span-2">{htmlCode[0]}</div>
+                <div className="font-semibold">Unicode:</div><div className="col-span-2"> {unicode[0]}</div>
             </div>
         </div>
     )
