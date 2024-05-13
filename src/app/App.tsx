2f4c1ea8cd1b7ed4ba7d76f2b77f4dcd1d895b20
KacperB <kacpero.barszczewski@gmail.com>
2024-05-13 17:21:51 +0200

    fix: page jump on page load

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,12 +8,15 @@ export const App = () => {
   const [category, onChange] = useState<UrlCategories>(UrlCategories.All);
 
   return (
-    <div className='min-h-screen flex flex-col justify-between bg-background text-typography dark:bg-background-dark dark:text-typography-dark  '>
-      <Header />
-      <Categories onChange={onChange}/>
-      <EmojiScreen category={category}/>
+    <>
+      <div className='min-h-screen flex flex-col justify-between bg-background text-typography dark:bg-background-dark dark:text-typography-dark  '>
+        <Header />
+        <Categories onChange={onChange} />
+        <EmojiScreen category={category} />
+      </div>
       <Footer />
-    </div>
+    </>
+
   )
 }
 
