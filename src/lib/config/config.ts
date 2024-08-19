e5d08341bc9791295f232f8e894639b47fa68766
KacperB <kacpero.barszczewski@gmail.com>
2024-08-20 00:02:20 +0200

    change api url

--- src/lib/config/config.ts
+++ src/lib/config/config.ts
@@ -1,6 +1,5 @@
 export const APP_CONFIG = {
     EMOJIAPI_URL: 'https://github.com/cheatsnake/emojihub',
     GITHUB_URL: 'https://github.com/KacperBarszczewski',
-    API_URL:'https://kacperbarszczewski.github.io/EmojiApi'
-    //API_URL:'http://localhost:4000/api'
+    API_URL:'https://kacperbarszczewski.github.io/Emoji_Page-React/api'
 }
