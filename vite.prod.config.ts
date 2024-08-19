fae656709aaedebeacc6ee1a4c9c96ffc8521608
KacperB <kacpero.barszczewski@gmail.com>
2024-08-20 00:14:36 +0200

    update path base

--- vite.prod.config.ts
+++ vite.prod.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Emoji_Page-React/',
+  base: '/Emoji_Page-React/page/',
   plugins: [react()],
   build: {
     sourcemap: false
